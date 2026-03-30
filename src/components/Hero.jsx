6f39f10afb00b2bbd2c5409c622a29d7568a0d08
MayasirSulduz <shaiksulduz238@gmail.com>
2026-03-30 19:44:52 +0530

    Setup done, Tailwindcss is working fine

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero(){
+    return(
+        <div>
+            <h1 className="text-white">
+                Hiiii
+            </h1>
+        </div>
+    )
+
+}
+
+export default Hero;
